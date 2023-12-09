6ec4fd65ca32c3f02ec41f2e428f2c6c219fd285
Crosstyan <cf595833532@gmail.com>
2023-12-10 01:03:57 +0800

    reset css

--- src/App.svelte
+++ src/App.svelte
@@ -43,10 +43,8 @@
 
 <style>
   #main-main {
-    width: 100vw;
-    height: 100vh;
-    position: absolute;
-    top: 0;
-    left: 0;
+    width: 100%;
+    height: 100%;
+    overflow-y: hidden;
   }
 </style>
