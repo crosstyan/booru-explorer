9f981d0848bfc20c781db957df488aeda102c7b4
Crosstyan <cf595833532@gmail.com>
2023-12-09 23:30:09 +0800

    remove deps

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,8 @@
 <script lang="ts">
-  import { Node, Svelvet, Minimap, Controls } from 'svelvet'
 </script>
 
 <main id="main">
-<Svelvet id="canvas" minimap >
-  <Node />
-  <Node id="alpha" bgColor="red" label="Default Node" />
-</Svelvet>
 </main>
 
 <style>
-  #main {
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    top: 0;
-    left: 0;
-  }
 </style>
