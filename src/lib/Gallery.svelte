698b752716fb1085069c8d2d332cb21c7de91532
Crosstyan <cf595833532@gmail.com>
2023-12-16 00:05:27 +0800

    front end is hard

--- src/lib/Gallery.svelte
+++ src/lib/Gallery.svelte
@@ -6,6 +6,7 @@ import { onMount, onDestroy, getContext } from "svelte"
 import pino from "pino"
 import { List } from "immutable"
 import Masonry from "svelte-bricks"
+import { cons } from "fp-ts/lib/ReadonlyNonEmptyArray"
 
 // https://github.com/sveltejs/svelte/issues/192#issuecomment-1288198489
 // https://svelteflow.dev/examples/styling/tailwind
@@ -55,19 +56,122 @@ const fetchNewImg = async () => {
   return promise
 }
 
-const [minColWidth, maxColWidth, gap] = [200, 1200, 10]
-let imgs: List<UrlEntry> = List()
+interface Post {
+  post_id: number
+  artist_tags: string[]
+  score: number
+  fav_count: number
+  general_tags: string[]
+  copyright_tags: string[]
+  characters_tags: string[]
+  created_at: string
+  file_url: string
+  preview_file_url: string
+}
+
+interface RawResponse {
+  names: string[]
+  rows: any[]
+}
+
+const fetchRandomPosts = async () => {
+  const api = import.meta.env.VITE_QUERY_API_ENDPOINT
+  const query = `--sql
+  WITH artist AS (SELECT *
+                FROM booru.artists_with_n_posts
+                ORDER BY random()
+                LIMIT 1),
+     artist_posts_with_tags_id AS (SELECT ap.post_id,
+                                          ap.score,
+                                          ap.fav_count,
+                                          ap.tag_ids,
+                                          ap.created_at,
+                                          ap.file_url,
+                                          ap.preview_file_url
+                                   FROM booru.view_modern_posts_illustration_only_extra ap
+                                   WHERE ap.tag_ids && ARRAY(SELECT tag_id FROM artist)),
+     artist_posts_with_tags AS (SELECT ap.post_id,
+                                       (SELECT array_agg(name)
+                                        FROM booru.tags
+                                        WHERE id = ANY (tag_ids)
+                                          AND category = 1) as artist_tags,
+                                       ap.score,
+                                       ap.fav_count,
+                                       (SELECT array_agg(name)
+                                        FROM booru.tags
+                                        WHERE id = ANY (tag_ids)
+                                          AND category = 0) as general_tags,
+                                       (SELECT array_agg(name)
+                                        FROM booru.tags
+                                        WHERE id = ANY (tag_ids)
+                                          AND category = 3) as copyright_tags,
+                                       (SELECT array_agg(name)
+                                        FROM booru.tags
+                                        WHERE id = ANY (tag_ids)
+                                          AND category = 4) as characters_tags,
+                                       ap.created_at,
+                                       ap.file_url,
+                                       ap.preview_file_url
+                                FROM artist_posts_with_tags_id as ap)
+  SELECT *
+  FROM artist_posts_with_tags
+  LIMIT 50;`
+  const req = fetch(api, {
+    method: "POST",
+    headers: {
+      "Content-Type": "text/plain",
+    },
+    body: query,
+  })
+  const res = await req
+  const json = await res.json()
+  return json
+}
+
+const rawRows2post = (raw: any[]) => {
+  const [
+    post_id,
+    artist_tags,
+    score,
+    fav_count,
+    general_tags,
+    copyright_tags,
+    characters_tags,
+    created_at,
+    file_url,
+    preview_file_url,
+  ] = raw
+  return {
+    id: post_id,
+    post_id,
+    artist_tags,
+    score,
+    fav_count,
+    general_tags,
+    copyright_tags,
+    characters_tags,
+    created_at,
+    file_url,
+    preview_file_url,
+  } as Post
+}
+
+const [minColWidth, maxColWidth, gap] = [50, 200, 5]
+let imgs: List<Post> = List()
 $: items = [...imgs]
+
+const addImg = async () => {
+  const posts = (await fetchRandomPosts()) as RawResponse
+  const newImgs = posts.rows.map(rawRows2post)
+  imgs = imgs.concat(newImgs)
+  console.log(imgs)
+}
 
 onMount(() => {
   ;(async () => {
-    const blob = await fetchNewImg()
-    const url = URL.createObjectURL(blob)
-    const id = (imgs.last()?.id ?? 0) + 1
-    imgs = imgs.push({
-      url,
-      id,
-    })
+    const raw = (await fetchRandomPosts()) as RawResponse
+    const posts = raw.rows.map(rawRows2post)
+    imgs = List(posts)
   })()
 })
 
@@ -75,18 +179,6 @@ const removeImg = () => {
   imgs = imgs.pop()
 }
 
-const addImg = () => {
-  ;(async () => {
-    const blob = await fetchNewImg()
-    const url = URL.createObjectURL(blob)
-    // https://www.reddit.com/r/sveltejs/comments/15baev9/why_is_updating_arrays_and_objects_designed_this/
-    const id = (imgs.last()?.id ?? 0) + 1
-    imgs = imgs.push({
-      url,
-      id,
-    })
-  })()
-}
 // https://www.npmjs.com/package/moveable
 // https://reactflow.dev/api-reference/components/node-resizer
 // https://github.com/xyflow/xyflow/blob/v11/packages/node-resizer/src/ResizeControl.tsx/#L27
@@ -94,8 +186,8 @@ const addImg = () => {
 
 <div class="gallery-frame">
   <Masonry let:item animate={false} {items} {minColWidth} {maxColWidth} {gap}>
-    <div class="flex">
-      <img src={item.url} class="flex-auto" />
+    <div class="flex-auto">
+      <img src={item.preview_file_url} />
     </div>
   </Masonry>
   <span class="tools absolute top-2 left-2 flex align-middle">
@@ -127,7 +219,11 @@ const addImg = () => {
   text-align: center;
   flex: auto;
   font-size: 0.75rem;
-  background-color: color-mix(in srgb,var(--node-background-color-default) 90%, white);
+  background-color: color-mix(
+    in srgb,
+    var(--node-background-color-default) 90%,
+    white
+  );
 }
 
 .float-btn:first-child {
@@ -141,7 +237,11 @@ const addImg = () => {
 }
 
 .float-btn:hover {
-  background-color: color-mix(in srgb,var(--node-background-color-default) 95%, white);
+  background-color: color-mix(
+    in srgb,
+    var(--node-background-color-default) 95%,
+    white
+  );
 }
 
 .float-btn:not(:last-child) {
